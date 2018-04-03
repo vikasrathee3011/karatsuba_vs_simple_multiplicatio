644b2f77a5f93eeb775b8238fbc437491bea3314
vikas rathee <vikasrathee3011@gmail.com>
2018-04-03 19:02:03 +0530

    Update simple_multiply.py

--- simple_multiply.py
+++ simple_multiply.py
@@ -1,10 +1,11 @@
 import timeit
+#importing module
 start_time = timeit.default_timer()
+#countdown starts
+
 def multiplication(A,B):
-    nu1 = A
-    nu2 = B
-    num1 = str(nu1)
-    num2 = str(nu2)
+    num1 = A
+    num2 = B
     print('----result----',int(num1)*int(num2))
 multiplication(9,1234)
 multiplication(7,12345)
@@ -13,5 +14,7 @@ multiplication(1234,5678)
 multiplication(12345,987)
 multiplication(3141592653589793238462643383279502884197169399375105820974944592
 ,2718281828459045235360287471352662497757247093699959574966967627)
+
 stop = timeit.default_timer()
+#countdown ends
 print('program time--',stop)
